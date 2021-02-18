0e7b955f8745c5b82140c43e52fdf44dc0b10ad1
geomio <geoff.millner22@gmail.com>
2021-02-18 10:47:14 -0800

    made working clickable js flash card grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,179 @@
 $(document).ready(function() {
-  $("button#but1").click(function() {
-    $(".hideText").removeClass();
-    $(".flashCard").addClass(".def1");
+  $(".term1").click(function() {
+    $(".define1").show();
+    $(".term1").hide();
   });
 });
+
+$(document).ready(function() {
+  $(".define1").click(function() {
+    $(".term1").show();
+    $(".define1").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term2").click(function() {
+    $(".define2").show();
+    $(".term2").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define2").click(function() {
+    $(".term2").show();
+    $(".define2").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term3").click(function() {
+    $(".define3").show();
+    $(".term3").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define3").click(function() {
+    $(".term3").show();
+    $(".define3").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term4").click(function() {
+    $(".define4").show();
+    $(".term4").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define4").click(function() {
+    $(".term4").show();
+    $(".define4").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term5").click(function() {
+    $(".define5").show();
+    $(".term5").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define5").click(function() {
+    $(".term5").show();
+    $(".define5").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term6").click(function() {
+    $(".define6").show();
+    $(".term6").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define6").click(function() {
+    $(".term6").show();
+    $(".define6").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term7").click(function() {
+    $(".define7").show();
+    $(".term7").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define7").click(function() {
+    $(".term7").show();
+    $(".define7").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term8").click(function() {
+    $(".define8").show();
+    $(".term8").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define8").click(function() {
+    $(".term8").show();
+    $(".define8").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term9").click(function() {
+    $(".define9").show();
+    $(".term9").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define9").click(function() {
+    $(".term9").show();
+    $(".define9").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term10").click(function() {
+    $(".define10").show();
+    $(".term10").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define10").click(function() {
+    $(".term10").show();
+    $(".define10").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term11").click(function() {
+    $(".define11").show();
+    $(".term11").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define11").click(function() {
+    $(".term11").show();
+    $(".define11").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".term12").click(function() {
+    $(".define12").show();
+    $(".term12").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".define12").click(function() {
+    $(".term12").show();
+    $(".define12").hide();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
